a2f79da06200f64d4fad98f181b6e9b3d28450ed
fokx <36614441+fokx@users.noreply.github.com>
2024-07-16 20:20:40 +0800

    init commit

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-# **Plugin Name** Plugin
+# Discourse CJK Formatter Plugin
 
-**Plugin Summary**
+A Discourse plugin that can reformat topic titles and post texts with mixed CJK and Latin characters and support auto space padding and punctuations correction.
+
+### Features
+
+Writing mixed CJK(Chinese, Japanese, Korean) characters and Latin characters can be a pain because of the special effort to take on the format. This plugin comes in to automatically reformat topic titles and post contents for you, utilizing [autocorrect-rb](https://github.com/huacnlee/autocorrect/tree/main/autocorrect-rb).
+
+Every time a topic/post is created or updated, its content (and title) will get:
+* added space between CJK (full width) and Latin (half-width) character
+* corrected punctuation adjacent to CJK character into full-width
+* corrected punctuation adjacent to Latin character into half-width
+
+### Configuration
+
+Just enable it in settings to make it work for new posts.
+
+To reformat all historic posts, use `rake`:
+```sh
+rake cjk:reformat
+rake posts:rebake
+```
+
+## Disclaimer
+Before use, be sure to learn about how the [`autocorrect`](https://github.com/huacnlee/autocorrect) tool will modify your content and backup your data before continue!
+
+## Acknowledgement
+* https://github.com/huacnlee/autocorrect
+* https://github.com/openSUSE-zh/discourse-cjk-text-spacing
 
 For more information, please see: **url to meta topic**
--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,45 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+gem 'autocorrect-rb', '2.11.1'
+gem 'rb_sys', '0.9.98'
+
+# name: discourse-cjk-formatter
+# about: Reformat topic titles and post texts with mixed CJK and Latin characters
 # meta_topic_id: TODO
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: https://github.com/fokx
+# url: https://github.com/fokx/discourse-cjk-formatter
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :discourse_cjk_formatter_enabled
 
 module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+  PLUGIN_NAME = "discourse-cjk-formatter"
 end
-
-require_relative "lib/my_plugin_module/engine"
 
 after_initialize do
   # Code which should run after Rails has finished booting
+
+  unless SiteSetting.discourse_cjk_formatter_enabled
+    NewPostManager.class_eval do
+      def initialize(user, args)
+        @user = user
+        args[:title] = AutoCorrect.format(args[:title]) if args[:title].present? and !args[:title].frozen?
+        args[:raw] = AutoCorrect.format(args[:raw]) if args[:raw].present? and !args[:raw].frozen?
+        @args = args.delete_if { |_, v| v.nil? }
+      end
+    end
+
+    PostRevisor.class_eval do
+      alias_method :old_revise!, :revise!
+
+      def revise!(editor, fields, opts = {})
+        fields[:raw] = AutoCorrect.format(fields[:raw]) if fields[:raw].present? and !fields[:raw].frozen?
+        fields[:title] = AutoCorrect.format(fields[:title]) if fields[:title].present? and !fields[:title].frozen?
+        old_revise!(editor, fields, opts)
+      end
+    end
+  end
+
 end
+
